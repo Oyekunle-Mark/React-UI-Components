77c970bcd9c3674a915c858c0f60bf002ace5c65
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-27 15:31:57 +0100

    Created the header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = ({ title, handle }) => (
+  <p className="title">
+    <b>{title}</b>{" "}
+    <small>
+      {handle} <span>.</span> 26 jan
+    </small>
+  </p>
+);
+
+export default HeaderTitle;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const ImageThumbnail = ({ url }) => (
+  <div className='thumbnail'>
+    <img
+      src={url}
+      alt="logo"
+    />
+  </div>
+);
+
+export default ImageThumbnail;
