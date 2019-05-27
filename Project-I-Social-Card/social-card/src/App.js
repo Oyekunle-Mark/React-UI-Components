f3dc3a62feea636b9a3b605e8c843f582b6726a2
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-27 17:08:13 +0100

    Styled the App component

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 import "./App.css";
 
 const App = () => (
   <div className='card'>
     <HeaderContainer />
     <CardContainer />
+    <Footer />
   </div>
 );
 
