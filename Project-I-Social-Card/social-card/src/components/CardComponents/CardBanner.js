3cccd8c5c8a65644bee5b43e3622564e700fc9b4
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-27 15:34:34 +0100

    Created the card components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardBanner = ({ url }) => {
+  const backgroundStyle = {
+    backgroundImage: `url(${url})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+  return (
+    <div
+      className="banner"
+      style={backgroundStyle}
+    />
+  );
+};
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 import './Card.css';
+
+const url = 'https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png';
+const header = 'Get started with React';
+const text = 'React makes it painless to create interactive UIs. Design simple views for each state in your application';
+
+const CardContainer = () => (
+  <div className='card-content'>
+    <CardBanner url={url} />
+    <CardContent header={header} text={text} />
+  </div>
+);
+
+export default CardContainer;
