fb1f22d71b077ed82b5b99144f92d2de98bf305c
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-28 11:40:46 +0100

    Refactored the footer component to use hooks instead of state

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,68 +1,48 @@
-import React from "react";
-import "./Footer.css";
+import React, { useState } from 'react';
+import './Footer.css';
 
-class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      retweet: 0,
-      like: 0,
-    };
+export default function Footer() {
+  const [retweet, updateRetweet] = useState(0);
+  const [like, updateLike] = useState(0);
 
-    this.likeHandler = this.likeHandler.bind(this);
-    this.retweetHandler = this.retweetHandler.bind(this);
-  }
+  const retweetHandler = () => {
+    updateRetweet(retweet + 1);
+  };
 
-  likeHandler() {
-    const newCount = this.state.like;
+  const likeHandler = () => {
+    updateLike(like + 1);
+  };
 
-    this.setState({
-      like: newCount + 1,
-    });
-  }
-
-  retweetHandler() {
-    const newCount = this.state.retweet;
-
-    this.setState({
-      retweet: newCount + 1,
-    });
-  }
-
-  render() {
-    return (
-      <footer>
-        <div className="icon">
-          <img
-            src="https://img.icons8.com/cotton/64/000000/speech-bubble-with-dots.png"
-            alt="comment"
-          />
-        </div>
-        <div className="icon">
-          <img
-            src="https://img.icons8.com/office/16/000000/retweet.png"
-            alt="retweet"
-            onClick={this.retweetHandler}
-          />
-          <span>{this.state.retweet === 0 ? " " : this.state.retweet}</span>
-        </div>
-        <div className="icon">
-          <img
-            src="https://img.icons8.com/dusk/64/000000/hearts.png"
-            alt="like"
-            onClick={this.likeHandler}
-          />
-          <span>{this.state.like === 0 ? " " : this.state.like}</span>
-        </div>
-        <div className="icon">
-          <img
-            src="https://img.icons8.com/nolan/64/000000/new-post.png"
-            alt="message"
-          />
-        </div>
-      </footer>
-    );
-  }
+  return (
+    <footer>
+      <div className="icon">
+        <img
+          src="https://img.icons8.com/cotton/64/000000/speech-bubble-with-dots.png"
+          alt="comment"
+        />
+      </div>
+      <div className="icon">
+        <img
+          src="https://img.icons8.com/office/16/000000/retweet.png"
+          alt="retweet"
+          onClick={retweetHandler}
+        />
+        <span>{retweet === 0 ? " " : retweet}</span>
+      </div>
+      <div className="icon">
+        <img
+          src="https://img.icons8.com/dusk/64/000000/hearts.png"
+          alt="like"
+          onClick={likeHandler}
+        />
+        <span>{like === 0 ? " " : like}</span>
+      </div>
+      <div className="icon">
+        <img
+          src="https://img.icons8.com/nolan/64/000000/new-post.png"
+          alt="message"
+        />
+      </div>
+    </footer>
+  );
 }
-
-export default Footer;
