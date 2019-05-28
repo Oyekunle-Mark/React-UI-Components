4b0e0cada45fca90820340503ebb08d8eea6a808
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-28 13:09:44 +0100

    Styled the calculator

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import NumberButton from './components/ButtonComponents/NumberButton';
-import ActionButton from './components/ButtonComponents/ActionButton';
-import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
-import './App.css';
+import React from "react";
+import NumberButton from "./components/ButtonComponents/NumberButton";
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='app'>
+    <div className="app">
       <CalculatorDisplay />
-      <NumberButton text='1' buttonStyle='number-button' />
-      <ActionButton text='clear' />
+      <div className="buttons">
+        <div className="keys">
+          <ActionButton text="clear" />
+          <NumberButton text="7" buttonStyle="number-button" />
+          <NumberButton text="8" buttonStyle="number-button" />
+          <NumberButton text="9" buttonStyle="number-button" />
+          <NumberButton text="4" buttonStyle="number-button" />
+          <NumberButton text="5" buttonStyle="number-button" />
+          <NumberButton text="6" buttonStyle="number-button" />
+          <NumberButton text="1" buttonStyle="number-button" />
+          <NumberButton text="2" buttonStyle="number-button" />
+          <NumberButton text="3" buttonStyle="number-button" />
+          <ActionButton text="0" />
+        </div>
+        <div className="operation">
+          <NumberButton text="/" buttonStyle="number-button" />
+          <NumberButton text="X" buttonStyle="number-button" />
+          <NumberButton text="-" buttonStyle="number-button" />
+          <NumberButton text="+" buttonStyle="number-button" />
+          <NumberButton text="=" buttonStyle="number-button" />
+        </div>
+      </div>
     </div>
   );
 };
