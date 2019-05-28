b4c25b4deed196b09cc4f50afd0a3a988fa7762b
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-28 12:36:58 +0100

    Built the display component

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import './App.css';
 
 const App = () => {
   return (
     <div className='app'>
+      <CalculatorDisplay />
       <NumberButton text='1' buttonStyle='number-button' />
+      <ActionButton text='clear' />
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => (
+  <p className='display'>0</p>
+);
+
+export default CalculatorDisplay;
